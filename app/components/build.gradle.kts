a028c9de06a8725f6ee8dbf4923ffcd16b8a1fc2
Arthur Araujo <arthur.araujo@tutanota.com>
2025-08-26 15:04:28 -0300

    Adapt java version to match the app's

--- app/components/build.gradle.kts
+++ app/components/build.gradle.kts
@@ -26,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -77,7 +77,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.word-view"
                 artifactId = "Components"
-                version = "1.1.0"
+                version = "1.1.1"
                 from(components["release"])
             }
         }
