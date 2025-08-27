c32308bf2c1e9c6ea711fee224fa9cf5d329d18f
Arthur Araujo <arthur.araujo@tutanota.com>
2025-08-27 01:39:41 -0300

    Add SongCard component

---
+++ app/components/src/main/java/cc/wordview/app/components/ui/SongCardPlaceholders.kt
@@ -0,0 +1,10 @@
+package cc.wordview.app.components.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Immutable
+
+@Immutable
+class SongCardPlaceholders(
+    @DrawableRes val noConnectionWhite: Int,
+    @DrawableRes val noConnectionDark: Int,
+)
